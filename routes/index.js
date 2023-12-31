5933b96c331cdd4ba0345a5ea898c964b4ff1c0c
Sourav Soni <sauravsoni8980@gmail.com>
2023-12-31 10:50:29 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+const userModel = require('./users');
+const postModel = require('./post');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+const upload = require('./multer');
+
+passport.use(new localStrategy(userModel.authenticate()))
+
+router.get('/', function(req, res, next) {
+  res.render('index', {nav: false});
+});
+router.get('/register', function(req, res, next) {
+  res.render('register', {nav: false});
+});
+router.get('/profile', isLoggedIn, async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user}).populate("posts");
+  res.render('profile', {user, nav: true});
+});
+router.get('/showpost', isLoggedIn, async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user}).populate("posts");
+  res.render('showpost', {user, nav: true});
+});
+router.get('/feed', isLoggedIn, async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user})
+  const posts = await postModel.find().populate("user")
+  res.render('feed', {user,posts, nav: true});
+});
+router.get('/add', isLoggedIn, async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user});
+  res.render('add', {user, nav: true});
+});
+router.post('/createpost', isLoggedIn, upload.single("postimage"), async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user});
+  const post = await postModel.create({
+    user: user._id,
+    title: req.body.title,
+    description: req.body.description,
+    image: req.file.filename
+  })
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+});
+router.post('/fileupload', isLoggedIn, upload.single("image"), async function(req, res, next) {
+  const user = await userModel.findOne({username: req.session.passport.user});
+  user.profileImage = req.file.filename;
+  await user.save();
+  res.redirect('/profile');
+});
+router.post('/register', function(req, res, next) {
+    const data = new userModel({
+      username: req.body.username,
+      name: req.body.name,
+      email: req.body.email
+    })
+    userModel.register(data, req.body.password)
+    .then(()=>{
+      passport.authenticate("local")(req,res, ()=>{
+        res.redirect("/profile")
+      })
+    })
+});
+
+router.post('/login', passport.authenticate("local", {
+  failureRedirect: "/",
+  successRedirect: "/profile",
+}), (req,res, next)=>{
+});
+
+router.get('/logout', (req, res, next)=>{
+  req.logout(function(err) {
+    if(err){
+       return next(err); 
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/")
+}
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+mongoose.connect("mongodb://localhost:27017/pinterest")
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  boards: {type: Array, default: []},
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post"
+    }
+  ]
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
